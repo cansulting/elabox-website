be3212b283020f96d89779016eebd4fa33c7130f
ademcan <ademcan@ademcan.net>
2020-03-08 16:07:59 +0100

    - Add new FAQ image - Update the flex to 3x2 - Add questions to the FAQ section

--- src/Faq.js
+++ src/Faq.js
@@ -1,12 +1,41 @@
 
 import React from "react";
 
-import faqHeader from './images/faq-img-1.png'
+import faqHeader from './images/faq-img-1-1920.png'
 
 export default function Faq() {
   return (
     <div>
       <img src={faqHeader} class="img-fluid"></img>
+      <div style={{backgroundColor: 'white', paddingLeft: 50, paddingBottom: 100, paddingRight: 50}}>
+      <font className="sectiontitle">What is the Elabox?</font><br/>
+      <span style={{fontSize:'20px'}}> The Elabox is your secure and safe blockchain computer running several Elastos services. You will be running the ELA mainchain in addition to the DID sidechain and the Carrier network. </span><br/>
+      <font className="sectiontitle">How do I earn rewards?</font><br/>
+      <span style={{fontSize:'20px'}}>
+        By running an Elabox you will automatically receive rewards from the Elabox SuperNode. The ElaboxSN is a super node running on behalf of Elabox owners on the DPoS consensus mechanis of Elastos. The project is maintained thanks to a collaboration with the Enter Elastos team. Every Elabox owner running their Elabox will automatically get rewarded. The amount of the reward is variable, for more information about how the reward system works, please refere to... <br/>
+      </span>
+      <font className="sectiontitle">How much will it costs me to run an Elabox?</font><br/>
+      <span style={{fontSize:'20px'}}>
+        After starting your Elabox the only cost that you are in charge will be the electricity bill. Based on calculation this amount should not exceed a few buck per year (estimated to be less than USD 10 for the US). <br/>
+      </span>
+      <font className="sectiontitle">Why would I run an Elabox?</font><br/>
+      <span style={{fontSize:'20px'}}>
+        Not your node not your ELA. By running your own nodes you won't need to rely on any other node to send your transactions. You will only trust your own node and therefore increase the security of your transactions as well as your precious ELA. The integrated wallet is creating the transaction in the Elabox and sending it directly from your own node. <br/>
+      </span>
+      <font className="sectiontitle">How secure is the Elabox companion app?</font><br/>
+      <span style={{fontSize:'20px'}}>
+        The only way to access your companion app is via the local network. In addition, it is also possible to access your companion app from outside of your local network with the TOR browser. Your Elabox will generate a random .onion address that you can use to access it when you are not home. Based on these two facts the Elabox is highly secure againy any attack. SSH connections are also disabled by default. <br/>
+      </span>
+      <font className="sectiontitle">How can I connect to my Elabox?</font><br/>
+      <span style={{fontSize:'20px'}}>
+        The companion webapp contains all the settings required to control your Elabox. However, if for some reasons you would like to connect to your Elabox directly you can plug a monitor and a keyboard to connect to it. At the command prompt you can use <b>elabox</b> as user and password to connect.<br/>
+      </span>
+      <font className="sectiontitle">What is included in the box?</font><br/>
+      <span style={{fontSize:'20px'}}>
+        You will receive an Elabox with additional 128 GB of storage, which is enough for several years of use. In addition, the box contains a USB-C power adapter (you might need an adapter based on your country) and an Ethernet cable to connect your Elabox to the Internet. <br/>
+      </span>
+      </div>
+
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -27,8 +27,10 @@ export default function Home() {
   <Row style={{padding:'70px', backgroundColor:'white'}}>
     <Col xs="12" style={{fontSize:'40px'}}>
       <center>
-        <img src={iconElabox} class="img-fluid" alt=""></img><br/>
-        Elabox<br></br> <b>The most secure way to access the Decentralized Web</b>
+        {/* <img src={iconElabox} class="img-fluid" alt=""></img><br/> */}
+        Elabox<br></br> 
+        <b>The most secure way to access the Decentralized Web</b> <br/>
+        <b>and keep your digital assets secure</b>
       </center>
     </Col>
   </Row>
@@ -49,7 +51,76 @@ export default function Home() {
 
   
 
-  <Row className="section2">
+  <Row style={{backgroundColor: 'white', alignSelf:'center', justifyContent:'center',textAlign:'center'}}>
+    <Col md="4">
+      <span  className="align-middle">
+        <center>
+          <img src={iconFullnode} class="img-fluid" alt="" style={{width: 130}}></img>
+        </center>
+      </span>
+      <br/>
+      <font className="sectiontitle">ELA mainchain</font><br/>
+      Your Elabox will be running a full ELA node. You will be able to use your own node to build, sign and send your transactions in a secure and trustless way.
+    </Col>
+    <Col md="4">
+    <span  className="align-middle">
+        <center>
+          <img src={iconCarrier} class="img-fluid" alt="" style={{width: 130}}></img>
+        </center>
+      </span>
+      <br/>
+      <font className="sectiontitle">Carrier</font><br/>
+      Your Elabox is an active element of the Carrier P2P network. You will be running an active Carrier node and strengthen the network by providing better security.
+    </Col>
+    <Col md="4">
+    <span  className="align-middle">
+        <center>
+          <img src={iconDid} class="img-fluid" alt="" style={{width: 130}}></img>
+        </center>
+      </span>
+      <br/>
+      <font className="sectiontitle">Decentralized ID</font><br/>
+      Your Elabox will be running a full DID node and let you connect to the decentralized ID network. The decentralized ID allows you to own your data and share only what you want to share with a DApp.
+    </Col>
+  </Row>
+
+
+  <Row style={{backgroundColor: 'white', alignSelf:'center', justifyContent:'center',textAlign:'center', paddingTop: 50, paddingBottom: 50}}>
+    <Col md="4">
+      <span  className="align-middle">
+        <center>
+          <img src={iconWallet} class="img-fluid" alt="" style={{width: 130}}></img>
+        </center>
+      </span>
+      <br/>
+      <font className="sectiontitle">Your Elabox, your wallet</font><br/>
+      You will be able to access your wallet via the dashboard and securely store your ELA in your Elabox. The Elabox is closed to any outside communication providing a better security.
+    </Col>
+    <Col md="4">
+    <span  className="align-middle">
+        <center>
+          <img src={iconRewards} class="img-fluid" alt="" style={{width: 130}}></img>
+        </center>
+      </span>
+      <br/>
+      <font className="sectiontitle">Receive automatic rewards</font><br/>
+      As an Elabox owner you will automatically take part in the DPoS consensus and receive rewards from the ElaboxSN (in collaboration with the Enter Elastos team)
+    </Col>
+    <Col md="4">
+    <span  className="align-middle">
+        <center>
+          <img src={iconDashboard} class="img-fluid" alt="" style={{width: 130}}></img>
+        </center>
+      </span>
+      <br/>
+      <font className="sectiontitle">Advanced dashboard</font><br/>
+      By connecting to your Elabox dashboard you will be able to have full control over your own node. You will also see the most recent statistics for the services running on your box.
+    </Col>
+  </Row>
+
+
+
+  {/* <Row className="section2">
     <Col md="6">
       <span  className="align-middle">
         <center>
@@ -63,12 +134,12 @@ export default function Home() {
       Your Elabox will be running a full ELA node. You will be able to use your own node to build, sign and send your transactions in a secure and trustless way.
       </span>
     </Col>
-  </Row>
+  </Row> */}
 
 
 
 
-  <Row className="section1">
+  {/* <Row className="section1">
   <div className="col-md-6 " style={{paddingLeft:'50px'}}>
       <span  className="align-middle">
       <font className="sectiontitle">Carrier</font><br/>
@@ -82,9 +153,9 @@ export default function Home() {
         </center>
       </span>
     </div>
-  </Row>
+  </Row> */}
 
-  <Row className="section2">
+  {/* <Row className="section2">
     <Col md="6">
       <span  className="align-middle">
         <center>
@@ -98,9 +169,9 @@ export default function Home() {
       Your Elabox will be running a full DID node and let you connect to the decentralized ID network. The decentralized ID allows you to own your data and share only what you want to share with a DApp.
       </span>
     </Col>
-  </Row>
+  </Row> */}
 
-  <Row className="section1">
+  {/* <Row className="section1">
   <div className="col-md-6 " style={{paddingLeft:'50px'}}>
       <span  className="align-middle">
       <font className="sectiontitle">Your Elabox, your wallet</font><br/>
@@ -114,9 +185,9 @@ export default function Home() {
         </center>
       </span>
     </div>
-  </Row>
+  </Row> */}
 
-  <Row className="section2">
+  {/* <Row className="section2">
     <Col md="6">
       <span  className="align-middle">
         <center>
@@ -130,9 +201,9 @@ export default function Home() {
       As an Elabox owner you will automatically take part in the DPoS consensus and receive rewards from the ElaboxSN (in collaboration with the Enter Elastos team)
       </span>
     </Col>
-  </Row>
+  </Row> */}
 
-  <Row className="section1" >
+  {/* <Row className="section1" >
   <div className="col-md-6 " style={{paddingLeft:'50px'}}>
       <span  className="align-middle">
       <font className="sectiontitle">Advanced dashboard</font><br/>
@@ -146,7 +217,7 @@ export default function Home() {
         </center>
       </span>
     </div>
-  </Row>
+  </Row> */}
 
 </div>    
   );
