24ba28cd76f68e95e4a4b24d959c3caf14221e6f
Bon Nicolai Mercado <bonhokage06@gmail.com>
2021-09-16 19:11:02 +0800

    change labels and make newsletter manual

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import './index.css';
-import HttpsRedirect from 'react-https-redirect';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import "./index.css"
+import HttpsRedirect from "react-https-redirect"
+import "bootstrap/dist/css/bootstrap.min.css"
 import {
   Navbar,
   NavbarBrand,
@@ -14,65 +10,111 @@ import {
   NavItem,
   NavLink,
   Row,
-  Col
-} from 'reactstrap';
-import logoElaboxWhite from './images/logo-wht.png'
-import Home from './Home'
-import Rewards from './Rewards'
-import Faq from './Faq'
-import Order from './Order'
-
+  Col,
+} from "reactstrap"
+import logoElaboxWhite from "./images/logo-wht.png"
+import Home from "./Home"
+import Rewards from "./Rewards"
+import Faq from "./Faq"
+import Order from "./Order"
 
 export default function App() {
-
-    return (
-      // <HttpsRedirect>
-      <Router>
-        <Navbar fixed="top" style={{backgroundColor:"#004E98",}}  className="navbar-dark">
-          <NavbarBrand href="/">
-            <img src={logoElaboxWhite} className="img-fluid" alt="" style={{height:'35px'}}></img>
-          </NavbarBrand>
-          <Nav className="mr-auto navbar-dark">
+  return (
+    // <HttpsRedirect>
+    <Router>
+      <Navbar
+        fixed="top"
+        style={{ backgroundColor: "#004E98" }}
+        className="navbar-dark"
+      >
+        <NavbarBrand href="/">
+          <img
+            src={logoElaboxWhite}
+            className="img-fluid"
+            alt=""
+            style={{ height: "35px" }}
+          ></img>
+        </NavbarBrand>
+        <Nav className="mr-auto navbar-dark">
           <NavItem>
-              <NavLink href="/rewards" style={{color:'white'}}>Rewards</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/faq" style={{color:'white'}}>FAQ</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://store.elabox.com" style={{color:'white'}}>Pre-order</NavLink>
-            </NavItem>
-          </Nav>
-        </Navbar>
+            <NavLink href="/rewards" style={{ color: "white" }}>
+              Rewards
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/faq" style={{ color: "white" }}>
+              FAQ
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://store.elabox.com" style={{ color: "white" }}>
+              Store
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              style={{ color: "white" }}
+              href="#"
+              onClick={() => {
+                document
+                  .querySelector(
+                    'div[data-form="f61462a7-16a6-11ec-96e5-06b4694bee2a"]'
+                  )
+                  .style.removeProperty("display")
+                document
+                  .querySelector(
+                    'div[data-form="f61462a7-16a6-11ec-96e5-06b4694bee2a"]'
+                  )
+                  .classList.add("active")
+              }}
+            >
+              Newsletter
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
 
-        <Switch>
+      <Switch>
         <Route path="/rewards">
-            <Rewards />
-          </Route>
-          <Route path="/faq">
-            <Faq />
-          </Route>
-          <Route path="/order">
-            <Order />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+          <Rewards />
+        </Route>
+        <Route path="/faq">
+          <Faq />
+        </Route>
+        <Route path="/order">
+          <Order />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
 
-        <Row className="footer">
-          <Col md="6" className="footerLeft">
-            Powered by <a style={{color: 'white'}} href="https://www.elastos.org/">Elastos</a> <br/>
-            Privacy Policy <br/>
-            Terms of use <br/>
-          </Col>
-          <Col md="6" className="footerRight">
-          <a style={{color: 'white'}} href="mailto:contact@elabox.com">Contact us</a> <br/>
-            <a style={{color: 'white'}} href="https://twitter.com/Elaboxdotcom">Twitter</a> <br/>
-            <a style={{color: 'white'}} href="https://t.me/elabox_official">Telegram</a> <br/>
-          </Col>
-        </Row>
-      </Router>
-      // </HttpsRedirect>
-    )
+      <Row className="footer">
+        <Col md="6" className="footerLeft">
+          Powered by{" "}
+          <a style={{ color: "white" }} href="https://www.elastos.org/">
+            Elastos
+          </a>{" "}
+          <br />
+          Privacy Policy <br />
+          Terms of use <br />
+        </Col>
+        <Col md="6" className="footerRight">
+          <a style={{ color: "white" }} href="mailto:contact@elabox.com">
+            Contact us
+          </a>{" "}
+          <br />
+          <a style={{ color: "white" }} href="https://twitter.com/Elaboxdotcom">
+            Twitter
+          </a>{" "}
+          <br />
+          <a style={{ color: "white" }} href="https://t.me/elabox_official">
+            Telegram
+          </a>{" "}
+          <br />
+        </Col>
+      </Row>
+    </Router>
+    // </HttpsRedirect>
+  )
 }
