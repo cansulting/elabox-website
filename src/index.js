3edb0570139d7bd35bbd4efd23ac3fffe6ff701e
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-02-14 04:49:11 +0800

    add partytown

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Row, Col, Container, Jumbotron } from 'reactstrap';
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Row,
-  Col
-} from 'reactstrap';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Partytown } from "@builder.io/partytown/react";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <>
+    <Partytown />
+    <App />
+  </>,
+  document.getElementById("root")
+);
